f3385522fd525b4e4b66fe01765af9fe8b84b0d5
Nicholas Istre <nick@teamworkadgroup.com>
2010-11-29 21:49:56 -0600

    Added comments to get_heightmap_func

--- heightadjust.py
+++ heightadjust.py
@@ -6,7 +6,9 @@ Created on Nov 29, 2010
 import math
 from PIL import ImageEnhance
 
-def get_heightmap_func(type = 'none'):
+def get_heightmap_func(type = None):
+    """Creates and returns a function and the ImageEnhance module class to go 
+    with it as a tuple, depending on the type passed in."""
     if type == 'heightlog':
         return [lambda z: (1/(1+math.exp(-1*(1.3*z/16)+6.0)))*.3, ImageEnhance.Brightness]
     if type == 'heightlog2':
